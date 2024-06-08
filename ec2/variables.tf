69e3b2b422a966e5a473b1ebd2fe7bbcf204a35c
Bhavana Doddabasappanavar <Bhavana.D@valtech.com>
2024-06-08 11:58:45 +0530

    add

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,18 +1,20 @@
 variable "instances" {
   description = "List of maps containing instance configurations"
   type = list(object({
-    instance_name   = string
-    instance_type   = string
-    ami             = string
-    key_name        = string
-    os_type       = string 
+    instance_name = string
+    instance_type = string
+    ami           = string
+    key_name      = string
+    os_type       = string
+    user_data     = string
+
   }))
 }
 
 variable "subnet_id" {
-  type = string
+  type        = string
   description = "ec2 subnet id"
-  
+
 }
 
 variable "security_group_id" {
